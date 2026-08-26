258ffa5d43f92979358b625ca268b92e8c84ad3a
dadda3ettaa-dotcom <dadda.3ettaa@gmail.com>
2026-08-26 06:26:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+      base: '/food-journey-app/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
